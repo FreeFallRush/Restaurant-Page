b21ba679168b7d02ba99dce94f94c1863f12185c
FreeFallRush <queenofcups.altbot@gmail.com>
2023-03-30 01:10:15 +0300

    Created menu-page.js in src
    Imported all the images and created a createMenuPage function

---
+++ src/menu-page.js
@@ -0,0 +1,15 @@
+import "./style.css";
+import Pic1 from "./img/Ponyo.gif";
+import Pic2 from "./img/SpongeBob-SquarePants.gif";
+import Pic3 from "./img/Mulan.gif";
+import Pic4 from "./img/lady-and-the-tramp.gif";
+import Pic5 from "./img/the-princess-and-the-frog.gif";
+import Pic6 from "./img/Alice-in-Wonderland.gif";
+import Pic7 from "./img/the-simpsons.gif";
+import Pic8 from "./img/buzz-cola-the-simpsons.gif";
+import Pic9 from "./img/duff-beer-the-simpsons.gif";
+import Pic10 from "./img/coffee-the-simpsons.gif";
+
+const createMenuPage = () => {};
+
+export default createMenuPage;
