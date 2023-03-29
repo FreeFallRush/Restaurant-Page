7e56bbd306393f72c8992b0ffda3884255e704c9
FreeFallRush <queenofcups.altbot@gmail.com>
2023-03-30 00:58:48 +0300

    Updated page-load.js
    Added all the info for main page: description, location, opening hours

--- src/page-load.js
+++ src/page-load.js
@@ -17,6 +17,47 @@ const createHomePage = () => {
   infoPic.src = Pic;
   main.appendChild(infoPic);
 
+  //add restaurant description
+  let textDiv = document.createElement("div");
+  textDiv.classList.add("info");
+  let description = document.createElement("p");
+  description.textContent = `Fill all your cartoon food cravings and childhood nostalgia in our restaurant!
+  We have everything here: from the legendary
+  "SpongeBob SquarePants" Krabby Patty to
+  "Alice in Wonderland"'s cookies that are just begging to be
+  eaten!`;
+  description.classList.add("description");
+
+  //add restaurant opening hours
+  let openingHours = document.createElement("div");
+  openingHours.classList.add("open-hours");
+  let heading1 = document.createElement("h3");
+  heading1.textContent = `Open Hours`;
+  openingHours.appendChild(heading1);
+  let openingHoursInfo = document.createElement("ul");
+  let weekdays = document.createElement("li");
+  weekdays.textContent = `Mon-Fri: every second of your free time`;
+  let weekend = document.createElement("li");
+  weekend.textContent = `weekend: 24/7`;
+
+  openingHoursInfo.appendChild(weekdays);
+  openingHoursInfo.appendChild(weekend);
+  openingHours.appendChild(openingHoursInfo);
+
+  //add restaurant location
+  let location = document.createElement("div");
+  location.classList.add("location");
+  let heading2 = document.createElement("h3");
+  heading2.textContent = `Location`;
+  location.appendChild(heading2);
+  let locationInfo = document.createElement("p");
+  locationInfo.textContent = `Imagination Street, Childhood Drawer`;
+  location.appendChild(locationInfo);
+
+  textDiv.appendChild(description);
+  textDiv.appendChild(openingHours);
+  textDiv.appendChild(location);
+  main.appendChild(textDiv);
   content.appendChild(main);
 };
 
